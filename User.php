8592fbf3745cef51e7a166a7c7d1af1f96d69de7
evan-sachot <evan.sachot@laplateforme.io>
2025-08-04 13:07:28 +0200

    Update User.php

--- User.php
+++ User.php
@@ -60,7 +60,51 @@ class User {
             return false;
         }
     }
-
+    public function update($login, $password,$email, $firstname,$lastname){
+        $sql = "UPDATE utilisateurs SET login = '$login', password = '$password', email = '$email', firstname = '$firstname', lastname = '$lastname' WHERE id = $this->sid";
+        global $conn;
+        if($conn->query($sql)){
+            $this->login = $login;
+            $this->email = $email;
+            $this->firstname = $firstname;
+            $this->lastname = $lastname;
+            $this->password = $password;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public function isconnected(){
+        return $this->sid !== null;
+    }
+    public function getAllInfos(){
+       global $conn;
+       $sql = "SELECT * FROM utilisateurs WHERE id = $this->sid";
+       $result = $conn->query($sql);
+       if($result->num_rows > 0){
+           $data = $result->fetch_assoc();
+           echo $data["logiqn"]."<br>";
+           echo $data["email"]."<br>";
+           echo $data["firstname"]."<br>";
+           echo $data["lastname"]."<br>";
+       }
+       else {
+           echo "No user found.";
+       }
+   }
+   public function getlogin(){
+         return $this->login;
+   }
+   public function getemail(){
+    return $this ->email;
+   }
+   public function getFirstname(){
+    return $this->firstname;
+   }
+   public function getLastname(){
+    return $this-> lastname ;
+   }
 }
 
 
